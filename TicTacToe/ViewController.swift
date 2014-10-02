e19a33fbe9b9616c86d8e09e3546fa5ce72a9a61
phitchcock <phitchcock@me.com>
2014-10-02 15:03:49 -0700

    taps recognized on first two labels

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Vars
+    var center: CGPoint?
+
     //IBOutlets
     @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var labelTwo: UILabel!
@@ -20,16 +23,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelSeven: UILabel!
     @IBOutlet weak var labelEight: UILabel!
     @IBOutlet weak var labelNine: UILabel!
-    
+    @IBOutlet weak var whichPlayerLabel: UILabel!
+
+    //IBActions
+    @IBAction func onLabelTapped(tapGesture: UITapGestureRecognizer) {
+        var point: CGPoint = tapGesture.locationOfTouch(0.1, inView: self.view)
+
+        self.findLabelUsingPoint(point)
+        println("\(point)")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    }
+
+    func findLabelUsingPoint(point: CGPoint) -> UILabel {
+
+        if (CGRectContainsPoint(labelOne.frame, point)) {
+
+            println("labelone")
+            return labelOne
+
+        }
+
+        else if (CGRectContainsPoint(labelTwo.frame, point)) {
+
+            println("labeltwo")
+            return labelOne
+        }
+
+        return labelOne
     }
 
 
