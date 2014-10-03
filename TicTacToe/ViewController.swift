2ac1a8cf0826f7701fe2005c4b8818015fca75fb
phitchcock <phitchcock@me.com>
2014-10-03 10:49:32 -0700

    added onButtonPressed action for button selection, connected all buttons to single action

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Variables
+    var startNumber = 1
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    //IBOutlets
+    @IBOutlet weak var gameButton0: UIButton!
+
+    //IBActions
+    @IBAction func onButtonPressed(sender: AnyObject) {
+
+        if (gameState[sender.tag] == 0) {
+
+            var changeImage = UIImage()
+
+            if (startNumber % 2 == 0) {
+
+                changeImage = UIImage(named: "x.png")
+                gameState[sender.tag] = 2
+                println("\(gameState)")
+
+            } else {
+
+                changeImage = UIImage(named: "o.png")
+                gameState[sender.tag] = 1
+                println("\(gameState)")
+
+            }
+
+            startNumber++
+
+            sender.setImage(changeImage, forState: .Normal)
+        }
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
